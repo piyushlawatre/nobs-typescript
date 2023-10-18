b080605c30701f52a1fecb2525a1ff0a1d337245
piyush-lawatre-coditas <piyushlawatre.dev@gmail.com>
2023-10-18 13:17:25 +0530

    [nobs-typescript] basic types

--- types.ts
+++ types.ts
@@ -7,7 +7,6 @@ let isUdemyInstructorPartner: boolean = true;
 // NOTE - CMD+K CMD+I use this vscode shortcut to infer the type of the variable
 /* -------------------------------- Consoles -------------------------------- */
 console.log("🚀 Instructor Full Name:", firstName + lastName);
-console.log("🚀 Instructor Contact Numsber:", phoneNumber);
+console.log("🚀 Instructor Contact Number:", phoneNumber);
 console.log("🚀 Udemy Instructor Partner:", isUdemyInstructorPartner);
-
 /* -------------------------------------------------------------------------- */
